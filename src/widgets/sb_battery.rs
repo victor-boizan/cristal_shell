05a9410f9f487a6f5b44b204e4f369f7c829cda3
Victor Boizan <victorboizan@protonmail.com>
2026-03-17 09:50:15 +0100

    add style to battery widget

--- src/widgets/sb_battery.rs
+++ src/widgets/sb_battery.rs
@@ -1,6 +1,8 @@
 use crate::messages::Message;
-use iced::widget::{container, text};
+use iced::color;
+use iced::widget::{container, container::Style, text};
 use iced::Element;
+use iced::Theme;
 use std::fs;
 use std::path::PathBuf;
 pub struct Battery {
@@ -20,6 +22,21 @@ impl Battery {
                 .unwrap();
     }
     pub fn view(&self) -> Element<'_, Message> {
-        container(text(self.lvl.clone())).into()
+        container(text(self.lvl.clone()))
+            .style(|theme: &Theme| Style {
+                text_color: Some(theme.clone().palette().text),
+                background: Some(iced::Background::Color(
+                    theme.clone().extended_palette().background.weak.color,
+                )),
+                border: iced::Border {
+                    color: color!(0x000000),
+                    width: 0.0,
+                    radius: iced::border::Radius::new(10),
+                },
+                shadow: iced::Shadow::default(),
+                snap: true, //IDK WTF is this.
+            })
+            .height(20)
+            .into()
     }
 }
