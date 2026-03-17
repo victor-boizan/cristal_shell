92db1f9287890e61463ffa7b5c36bf61207fb4b4
Victor Boizan <victorboizan@protonmail.com>
2026-03-17 09:43:01 +0100

    add style to clock

--- src/widgets/sb_clock.rs
+++ src/widgets/sb_clock.rs
@@ -1,7 +1,9 @@
 use crate::messages::Message;
 use chrono::Local;
-use iced::widget::{column, container, text};
+use iced::color;
+use iced::widget::{column, container, container::Style, text};
 use iced::Element;
+use iced::Theme;
 pub struct Clock {}
 
 impl Clock {
@@ -11,6 +13,19 @@ impl Clock {
             text(time.format("%H").to_string()),
             text(time.format("%M").to_string()),
         ])
+        .style(|theme: &Theme| Style {
+            text_color: Some(theme.clone().palette().text),
+            background: Some(iced::Background::Color(
+                theme.clone().extended_palette().background.weak.color,
+            )),
+            border: iced::Border {
+                color: color!(0x000000),
+                width: 0.0,
+                radius: iced::border::Radius::new(10),
+            },
+            shadow: iced::Shadow::default(),
+            snap: true, //IDK WTF is this.
+        })
         .into()
     }
 }
